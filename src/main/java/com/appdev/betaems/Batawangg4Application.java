24dc57838d7015b31d008302ac5e5cff18d6ab2a
Zly Batucan <zlyhansonbatucan@gmail.com>
2025-12-03 19:09:50 +0800

    applications.properties and batawangg4application.java transfer into /betaems
    transfer: batawangg4application.java transfer into /betaems

--- src/main/java/com/appdev/betaems/Batawangg4Application.java
+++ src/main/java/com/appdev/betaems/Batawangg4Application.java
@@ -1,4 +1,4 @@
-package com.appdev.betaems.batawangg4;
+package com.appdev.betaems;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
